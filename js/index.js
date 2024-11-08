6ebe6344fcd0bbf0b1de8ca84162163ca77c4353
Максимилиан <max4tim@bk.ru>
2024-11-08 18:53:15 +0300

    keyboard triggers moving

--- js/index.js
+++ js/index.js
@@ -1,14 +1,24 @@
 function drawFrame(time) {
     let verse = document.getElementById('verse-1');
 
-    verse.style.top = time/100 + 'px';
-    document.getElementById('qwe').innerHTML = "good " + time + " ee";
+    let positionBegin = +window.innerHeight;
+    // let positionEnd = +0;
+    let positionEnd = -verse.offsetHeight;
+    // document.getElementById('qwe').innerHTML = "good " + verse.offsetHeight + " ee";
+
+    let partPassed = time / 3000;
+
+    verse.style.top = (
+        +positionBegin * (+1 - partPassed) +
+        +positionEnd * (+partPassed)
+    ) + 'px';
 
 }
 
 function main() {
     let timerUpdateFrame;
-    let timeStart = Date.now();
+    // let timeStart = Date.now();
+    let timeStart;
 
     function updateFrame() {
         let time = Date.now() - timeStart;
@@ -20,8 +30,19 @@ function main() {
         drawFrame(time)
     }
 
-    timerUpdateFrame = setInterval(updateFrame, 20);
-    drawFrame(500);
+    function processKeydown(event) {
+        timerUpdateFrame = setInterval(updateFrame, 5);
+        timeStart = Date.now()
+    }
+
+    document.onkeydown = processKeydown;
+
+    // document.onclick = function() {
+    //     clearInterval(timerUpdateFrame);
+    // };
+
+
+    // drawFrame(500);
 }
 
 main();
