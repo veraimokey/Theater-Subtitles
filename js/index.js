cab5fc30a324c1e5a10bb207dba458c657778454
Максимилиан <max4tim@bk.ru>
2024-11-11 00:05:52 +0300

    js changes html

--- js/index.js
+++ js/index.js
@@ -1,3 +1,33 @@
+'use stick'
+
+class Verse {
+    text;
+    duration;
+    key;
+
+    active = false;
+    timeStart;
+
+    constructor() {
+    }
+
+    activate() {
+        if (this.active == true) return;
+
+        this.active = true;
+        this.timeStart = Date.now();
+
+        document.getElementById('verses').insertAdjacentHTML('beforeend',
+            '<div id=verse-' + this.key + '" class="verse">' +
+            this.text
+            + '</div>'
+        );
+    }
+
+}
+
+let verses = new Array();
+
 function drawFrame(time) {
     let verse = document.getElementById('verse-1');
 
@@ -6,7 +36,7 @@ function drawFrame(time) {
     let positionEnd = -verse.offsetHeight;
     // document.getElementById('qwe').innerHTML = "good " + verse.offsetHeight + " ee";
 
-    let partPassed = time / 3000;
+    let partPassed = time / 6000;
 
     verse.style.top = (
         +positionBegin * (+1 - partPassed) +
@@ -20,19 +50,42 @@ function main() {
     // let timeStart = Date.now();
     let timeStart;
 
+
+    verses.push(new Verse());
+    verses.at(-1).text =
+        "\
+        <p>\
+            Вот и всё, Мать Земля, мы отбились от предков <br>\
+            Поменяли обычаи и простили заветы <br>\
+            Мы забыли рождение и в лицо вам хохочем <br>\
+            Мы краснеем от солнца и чернеем от ночи <br>\
+        </p>\
+        <p>\
+            Здравствуй, Небо Отец, мы лежим под тобою <br>\
+            Знаем, совести эхо ты навечно укроешь <br>\
+            От бесплотных надежд и от душ неспасённых <br>\
+            И от всех тайн людских, нам судьбой принесённых <br>\
+        </p>\
+        ";
+    verses.at(-1).duration = 6000;
+    verses.at(-1).key = 1;
+
     function updateFrame() {
         let time = Date.now() - timeStart;
 
-        if (time > 3000) {
+        if (time > 6000) {
             clearInterval(timerUpdateFrame);
         }
+        // document.getElementById('qwe').innerHTML = "good " + time + " ee";
 
-        drawFrame(time)
+        drawFrame(time);
     }
 
     function processKeydown(event) {
         timerUpdateFrame = setInterval(updateFrame, 5);
-        timeStart = Date.now()
+        timeStart = Date.now();
+
+        verses.at(-1).activate();
     }
 
     document.onkeydown = processKeydown;
