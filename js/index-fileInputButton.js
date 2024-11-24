413692406797da22f60637642b7191f8a82fd833
Максимилиан <max4tim@bk.ru>
2024-11-24 03:44:49 +0300

    fileInputButton focus indicator

--- js/index-fileInputButton.js
+++ js/index-fileInputButton.js
@@ -5,3 +5,11 @@ let button = document.getElementById("fileInput");
 button.addEventListener('click', (e) => {
     button.blur();
 });
+
+button.addEventListener('focus', (e) => {
+    button.nextElementSibling.classList.add('focusedButton');
+});
+
+button.addEventListener('blur', (e) => {
+    button.nextElementSibling.classList.remove('focusedButton');
+});
